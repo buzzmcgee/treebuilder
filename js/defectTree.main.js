4f0cf2d6f8ecec6388406e84bb6dbfce18bad86b
buzzmcgee <didjumoi@googlemail.com>
2023-10-23 23:37:37 +0200

    Add tree filter object

---
+++ js/defectTree.main.js
@@ -0,0 +1,419 @@
+const tMap = {
+    'schaden': [
+        {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 1,
+            "id": 1264,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1264, "id": 3726, "lang": "de", "name": "Motor/Getriebe"}}
+        }
+    ],
+    'mangel': [
+        {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 1,
+            "id": 1264,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1264, "id": 3726, "lang": "de", "name": "Motor/Getriebe"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 2,
+            "id": 1269,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1269, "id": 3731, "lang": "de", "name": "Fahrwerk/Bremse"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 3,
+            "id": 1274,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1274, "id": 3736, "lang": "de", "name": "Beleuchtung/Elektrik"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 4,
+            "id": 1279,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1279, "id": 3741, "lang": "de", "name": "Heizung/Klimaanlage"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 5,
+            "id": 1284,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1284, "id": 3746, "lang": "de", "name": "Fahrgastraum"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 0,
+            "sort": 6,
+            "id": 1290,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1290, "id": 3752, "lang": "de", "name": "Sachschäden"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1264,
+            "sort": 1,
+            "id": 1265,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1265, "id": 3727, "lang": "de", "name": "Fehlermeldungen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1264,
+            "sort": 2,
+            "id": 1266,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1266, "id": 3728, "lang": "de", "name": "Warnleuchten"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1264,
+            "sort": 3,
+            "id": 1267,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1267, "id": 3729, "lang": "de", "name": "Geräusche"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1264,
+            "sort": 4,
+            "id": 1268,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1268, "id": 3730, "lang": "de", "name": "Vibrationen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1269,
+            "sort": 5,
+            "id": 1270,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1270, "id": 3732, "lang": "de", "name": "Fehlermeldungen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1269,
+            "sort": 6,
+            "id": 1271,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1271, "id": 3733, "lang": "de", "name": "Warnleuchten"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1269,
+            "sort": 7,
+            "id": 1272,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1272, "id": 3734, "lang": "de", "name": "Geräusche"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1269,
+            "sort": 8,
+            "id": 1273,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1273, "id": 3735, "lang": "de", "name": "schlechte Wirkung"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1274,
+            "sort": 9,
+            "id": 1275,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1275, "id": 3737, "lang": "de", "name": "Fehlermeldungen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1274,
+            "sort": 10,
+            "id": 1276,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1276, "id": 3738, "lang": "de", "name": "Warnleuchten"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1274,
+            "sort": 11,
+            "id": 1277,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1277, "id": 3739, "lang": "de", "name": "Funktionsstörungen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1274,
+            "sort": 12,
+            "id": 1278,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1278, "id": 3740, "lang": "de", "name": "ausgefallene Anzeigen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1279,
+            "sort": 13,
+            "id": 1280,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1280, "id": 3742, "lang": "de", "name": "Fehlermeldungen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1279,
+            "sort": 14,
+            "id": 1281,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1281, "id": 3743, "lang": "de", "name": "Warnleuchten"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1281,
+            "sort": 15,
+            "id": 1282,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1282, "id": 3744, "lang": "de", "name": "Funktionsstörungen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1279,
+            "sort": 16,
+            "id": 1283,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1283, "id": 3745, "lang": "de", "name": "Geräusche"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1284,
+            "sort": 17,
+            "id": 1285,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1285, "id": 3747, "lang": "de", "name": "ausgefallene Anzeigen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1284,
+            "sort": 18,
+            "id": 1286,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1286, "id": 3748, "lang": "de", "name": "Innenbeleuchtung"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1284,
+            "sort": 19,
+            "id": 1287,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1287, "id": 3749, "lang": "de", "name": "Klappergeräusche"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1284,
+            "sort": 20,
+            "id": 1288,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1288, "id": 3750, "lang": "de", "name": "Sitze"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1284,
+            "sort": 21,
+            "id": 1289,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1289, "id": 3751, "lang": "de", "name": "Haltestangen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1290,
+            "sort": 22,
+            "id": 1291,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1291, "id": 3753, "lang": "de", "name": "Außen"}}
+        }, {
+            "code": "",
+            "mediaidfk": "",
+            "parentid": 1290,
+            "sort": 23,
+            "id": 1292,
+            "deleted": 0,
+            "filename": "",
+            "lang": {"de": {"klassifizierungidfk": 1292, "id": 3754, "lang": "de", "name": "Innen"}}
+        }
+    ]
+};
+
+const tTypes = ['schaden', 'mangel'];
+
+const defectTree = {
+    map: [],
+    type: '',
+    refTree: {},
+    refGroups: {},
+
+    /**
+     * Returns object-instance
+     * @param map
+     * @param type
+     * @returns {defectTree}
+     */
+    init: function (map, type) {
+        // clone object = create new instance
+        let self = Object.assign({}, this);
+
+        // build base
+        self.type = type;
+        self.currentSelection = [0];
+
+        self.update(map);
+
+        // console.log('trunk.init', self);
+        return self;
+    },
+
+    /**
+     * Return level-separated entry list for selection
+     * returns only ground level, if validation fails
+     * @param selection
+     * @returns {*[]}
+     */
+    getSelectionLevels: function (selection) {
+        if (!this.validateSelection(selection, this.refTree)) {
+            console.log('validation failed for ', selection);
+
+            return this.refGroups[0];
+        }
+
+        let levels = [];
+
+        selection.forEach(levelParentId => {
+            let foundGroup = this.refGroups[levelParentId];
+
+            if (foundGroup !== undefined) {
+                levels.push(foundGroup);
+            }
+        });
+
+        return levels;
+    },
+
+    /**
+     * Update used Map
+     * @param map
+     */
+    update: function(map) {
+        this.map = map;
+
+        this.refTree = {0: this._buildReferenceTree(this.map, 0)};
+        this.refGroups = this._buildReferenceGroups(this.map);
+    },
+
+    /**
+     * Validate selection, using current map
+     * @param selection
+     * @param currentTree
+     * @returns {boolean|*}
+     */
+    validateSelection: function (selection, currentTree) {
+        let currentSelection = [...selection];
+        let currentFocus = currentSelection.shift();
+
+        if (currentTree[currentFocus] === undefined) {
+            return false;
+        }
+
+        if (Object.keys(currentTree[currentFocus]).length === 0 || currentSelection.length === 0) {
+            return true;
+        }
+
+        return this.validateSelection(currentSelection, currentTree[currentFocus]);
+    },
+
+    /**
+     * Create parental inheritance overview
+     * @param map
+     * @param parentId
+     * @returns {{}}
+     * @private
+     */
+    _buildReferenceTree: function (map, parentId) {
+        let level = {};
+        let children = map.filter(item => {
+            return item.parentid === parentId;
+        });
+
+        let others = map.filter(item => {
+            return item.parentid !== parentId;
+        });
+
+        children.forEach(item => {
+            level[item.id] = this._buildReferenceTree(others, item.id);
+        });
+
+        return level;
+    },
+
+    /**
+     * Create parent-group-map
+     * @param map
+     * @returns {{}}
+     * @private
+     */
+    _buildReferenceGroups: function (map) {
+        let groups = {};
+
+        map.forEach(parent => {
+            groups[parent.parentid] = map.filter(item => {
+                return item.parentid === parent.parentid;
+            });
+        });
+
+        return groups;
+    },
+};
+
+let trees = [];
+tTypes.forEach(type => {
+    let tree = defectTree.init(tMap[type], type);
+
+    // override example
+    // tree.validateSelection = function (selection, currentTree) {return true};
+
+    trees.push(tree);
+});
+
+console.log(trees);
